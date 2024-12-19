b0b6b825add23a743178a834157703d7bc4dfbc6
Nalakaroshan <nalakaroshan666@gmail.com>
2024-12-19 22:08:03 +0530

    Initiate front end

---
+++ src/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Create (){
+    return(
+        <div className="create_from">
+            <input type="text" Name="" id="" placeholder="Enter Task"/>
+            <button type="button">Add</button>
+        </div>
+    )
+}
+
+export default Create
